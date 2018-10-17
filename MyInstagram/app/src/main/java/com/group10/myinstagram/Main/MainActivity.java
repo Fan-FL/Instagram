2c6710fbc096830c76dfa33745e39c9d9006ff26
Sirui Yan <1511374803@qq.com>
2018-10-18 01:20:05 +1100

    add finding bluetooth devices

--- MyInstagram/app/src/main/java/com/group10/myinstagram/Main/MainActivity.java
+++ MyInstagram/app/src/main/java/com/group10/myinstagram/Main/MainActivity.java
@@ -2,25 +2,47 @@ package com.group10.myinstagram.Main;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.group10.myinstagram.Bluetooth.BluetoothActivity;
 import com.group10.myinstagram.Login.LoginActivity;
 import com.group10.myinstagram.R;
 import com.group10.myinstagram.Utils.BottomNavigationViewHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private Context mContext = MainActivity.this;
     private static final int ACTIVITY_NUM = 0;
+
+    //bluetooth
+    private static final int REQUEST_BLUETOOTH_PERMISSIONS = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+    private BluetoothAdapter bluetoothAdapter;
+    private static final String[] BLUE_PERMISSIONS = {
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
     //firebase
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
@@ -32,6 +54,15 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG,"onCreate: starting.");
         setupFirebaseAuth();
         setupBottomNavigationView();
+
+        Button btnBluetooth = (Button) findViewById(R.id.btn_bluetooth);
+
+        btnBluetooth.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: find pair.");
+                startBluetoothSensor();
+            }
+        });
     }
 
     private void setupBottomNavigationView(){
@@ -93,5 +124,94 @@ public class MainActivity extends AppCompatActivity {
         if(mAuthStateListener != null){
             mAuth.removeAuthStateListener(mAuthStateListener);
         }
+    }
+
+    //Bluetooth
+
+    public void startBluetoothSensor(){
+
+        // This only targets API 23+
+        // check permission using a thousand lines (Google is naive!)
+
+        Log.d(TAG, "Start bluetooth sensors");
+        if (!hasPermissionsGranted(BLUE_PERMISSIONS)) {
+            requestBluePermissions(BLUE_PERMISSIONS);
+            return;
+        }
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter == null)
+        {
+            Log.d(TAG, "Device has no bluetooth");
+            return;
+        }
+
+        // ask users to open bluetooth
+        if (bluetoothAdapter.isEnabled()==false){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+
+        // make this device visible to others for 3000 seconds
+        Intent discoverableIntent = new
+                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 3000);
+        startActivity(discoverableIntent);
+
+        //find devices
+        Intent intent = new Intent(mContext, BluetoothActivity.class);
+        startActivity(intent);
+    }
+
+    // check if app has a list of permissions, then request not-granted ones
+
+    public void requestBluePermissions(String[] permissions) {
+        Log.d(TAG, "Request bluetooth permissions");
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(permission);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_BLUETOOTH_PERMISSIONS);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_BLUETOOTH_PERMISSIONS:
+                Log.d(TAG, "Request permission");
+
+                if (grantResults.length > 0) {
+                    for (int grantResult : grantResults) {
+                        if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                            Log.d(TAG, "one or more permission denied");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "all permissions granted");
+
+                }
+
+        }
+    }
+
+
+    private boolean hasPermissionsGranted(String[] permissions) {
+        Log.d(TAG, "Check permission granted");
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
