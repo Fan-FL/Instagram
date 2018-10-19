dc4de1adb97bf0d0dee4a4b94fa490235861353c
DESKTOP-LV06VNT\li1403 <jingyil14@student.unimelb.edu.au>
2018-10-19 18:55:54 +1100

    add list adapter

--- MyInstagram/app/src/main/java/com/group10/myinstagram/Main/MainActivity.java
+++ MyInstagram/app/src/main/java/com/group10/myinstagram/Main/MainActivity.java
@@ -9,13 +9,31 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.group10.myinstagram.Login.LoginActivity;
+import com.group10.myinstagram.Models.Comment;
+import com.group10.myinstagram.Models.Photo;
 import com.group10.myinstagram.R;
 import com.group10.myinstagram.Utils.BottomNavigationViewHelper;
+import com.group10.myinstagram.Utils.UniversalImageLoader;
+import com.group10.myinstagram.Utils.UserfeedListAdapter;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -24,16 +42,128 @@ public class MainActivity extends AppCompatActivity {
     //firebase
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    //vars
+    private ArrayList<Photo> mPhotos;
+    private ArrayList<String> mFollowing;
+    private ListView mListView;
+    private UserfeedListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreate: starting.");
+
         setupFirebaseAuth();
         setupBottomNavigationView();
+        initImageLoader();
+
+        mListView = (ListView) findViewById(R.id.listView);
+        mFollowing = new ArrayList<>();
+        mPhotos = new ArrayList<>();
+
+        getFollowing();
     }
 
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(mContext);
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void getFollowing(){
+        Log.d(TAG, "getFollowing: searching for following");
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Query query = reference
+                .child(getString(R.string.dbname_following))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                    Log.d(TAG, "onDataChange: found user: " +
+                            singleSnapshot.child(getString(R.string.field_user_id)).getValue());
+
+                    mFollowing.add(singleSnapshot.child(getString(R.string.field_user_id)).getValue().toString());
+                }
+                //get the photos
+                getPhotos();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getPhotos(){
+        Log.d(TAG, "getPhotos: getting photos");
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        for(int i = 0; i < mFollowing.size(); i++){
+            final int count = i;
+            Query query = reference
+                    .child(getString(R.string.dbname_user_photos))
+                    .child(mFollowing.get(i))
+                    .orderByChild(getString(R.string.field_user_id))
+                    .equalTo(mFollowing.get(i));
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+
+                        Photo photo = new Photo();
+                        Map<String, Object> objectMap = (HashMap<String, Object>) singleSnapshot.getValue();
+
+                        photo.setCaption(objectMap.get(getString(R.string.field_caption)).toString());
+                        photo.setTags(objectMap.get(getString(R.string.field_tags)).toString());
+                        photo.setPhoto_id(objectMap.get(getString(R.string.field_photo_id)).toString());
+                        photo.setUser_id(objectMap.get(getString(R.string.field_user_id)).toString());
+                        photo.setDate_created(objectMap.get(getString(R.string.field_date_created)).toString());
+                        photo.setImage_path(objectMap.get(getString(R.string.field_image_path)).toString());
+
+                        ArrayList<Comment> comments = new ArrayList<Comment>();
+                        for (DataSnapshot dSnapshot : singleSnapshot
+                                .child(getString(R.string.field_comments)).getChildren()){
+                            Comment comment = new Comment();
+                            comment.setUser_id(dSnapshot.getValue(Comment.class).getUser_id());
+                            comment.setComment(dSnapshot.getValue(Comment.class).getComment());
+                            comment.setDate_created(dSnapshot.getValue(Comment.class).getDate_created());
+                            comments.add(comment);
+                        }
+
+                        photo.setComments(comments);
+                        mPhotos.add(photo);
+                    }
+                    if(count >= mFollowing.size() -1){
+                        //display our photos
+                        displayPhotos();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    private void displayPhotos(){
+        if(mPhotos != null){
+            Collections.sort(mPhotos, new Comparator<Photo>() {
+                @Override
+                public int compare(Photo o1, Photo o2) {
+                    return o2.getDate_created().compareTo(o1.getDate_created());
+                }
+            });
+
+            mAdapter = new UserfeedListAdapter(mContext, R.layout.layout_userfeed_listitem, mPhotos);
+            mListView.setAdapter(mAdapter);
+        }
+    }
+
+    // navigation bar
     private void setupBottomNavigationView(){
         Log.d(TAG,"setupBottomNavigationView: setting up BottomNavigationView");
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
