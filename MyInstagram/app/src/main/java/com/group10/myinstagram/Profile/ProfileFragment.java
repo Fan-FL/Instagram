e090c94376435db1be8839c33cb2ba9ee9c4b529
Fan Li <funlee1314@gmail.com>
2018-10-21 00:03:48 +1100

    update view profile

--- MyInstagram/app/src/main/java/com/group10/myinstagram/Profile/ProfileFragment.java
+++ MyInstagram/app/src/main/java/com/group10/myinstagram/Profile/ProfileFragment.java
@@ -223,29 +223,6 @@ public class ProfileFragment extends Fragment {
                     photo.setUser_id(objectMap.get(getString(R.string.field_user_id)).toString());
                     photo.setDate_created(objectMap.get(getString(R.string.field_date_created)).toString());
                     photo.setImage_path(objectMap.get(getString(R.string.field_image_path)).toString());
-
-                    ArrayList<Like> likes = new ArrayList<Like>();
-                    for (DataSnapshot dSnapshot : singleSnapshot
-                            .child(getString(R.string.field_likes)).getChildren()){
-                        Log.d(TAG, "onDataChange: main activity: load likes:" +
-                                dSnapshot.getValue(Like.class).getUsername());
-                        Like like = new Like();
-                        like.setUsername(dSnapshot.getValue(Like.class).getUsername());
-                        likes.add(like);
-                    }
-                    photo.setLikes(likes);
-
-                    ArrayList<Comment> comments = new ArrayList<Comment>();
-                    for (DataSnapshot dSnapshot : singleSnapshot
-                            .child(getString(R.string.field_comments)).getChildren()){
-                        Comment comment = new Comment();
-                        comment.setUser_id(dSnapshot.getValue(Comment.class).getUser_id());
-                        comment.setComment(dSnapshot.getValue(Comment.class).getComment());
-                        comment.setDate_created(dSnapshot.getValue(Comment.class).getDate_created());
-                        comments.add(comment);
-                    }
-
-                    photo.setComments(comments);
                     photos.add(photo);
                 }
                 int gridWidth = getResources().getDisplayMetrics().widthPixels;
